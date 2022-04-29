bae0cf6e801d4afe31c6362b31bcf9a938648434
sazzad258 <92928794+sazzad258@users.noreply.github.com>
2022-04-30 00:11:20 +0600

    Add files via upload

---
+++ min.js
@@ -0,0 +1,18 @@
+$('#tabs-nav li:first-child').addClass('active');
+$('.tab-content').hide();
+$('.tab-content:first').show();
+
+$('#tabs-nav li').click(function(){
+  $('#tabs-nav li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
+
+
+$( function() {
+  $( "#draggable" ).draggable();
+} );
